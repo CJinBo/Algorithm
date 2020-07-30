91b7e552fa0e26f5a63eed0a46007c79e1ec6575
jinbo.cao <jinbo.cao@ly.com>
2020-07-30 23:16:54 +0800

    修改对数器

--- src/one/ArrayRandomTools.java
+++ src/one/ArrayRandomTools.java
@@ -2,8 +2,9 @@ package one;
 
 import java.util.Arrays;
 
-public class testUtil {
+public class ArrayRandomTools {
 
+    //
     public static void selectionSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
@@ -23,12 +24,12 @@ public class testUtil {
         arr[j] = tmp;
     }
 
-    // for test
+    // for test  绝对真确的一个排序
     public static void comparator(int[] arr) {
         Arrays.sort(arr);
     }
 
-    // for test
+    // for test  随机数组生成器
     public static int[] generateRandomArray(int maxSize, int maxValue) {
         int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
         for (int i = 0; i < arr.length; i++) {
@@ -81,7 +82,7 @@ public class testUtil {
 
     // for test
     public static void main(String[] args) {
-        int testTime = 500000;
+        int testTime = 50;
         int maxSize = 100;
         int maxValue = 100;
         boolean succeed = true;
